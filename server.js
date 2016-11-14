d2b791d300549c4a9f5413da65a71725c2fc3b4a
lyninx <dennis.pacewicz@gmail.com>
2016-11-14 18:18:42 -0500

    add nteractions article to routing

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express()
 var partners_network = express()
+var nteractions = express()
 var mongoose = require('mongoose')
 var handlebars = require('express-handlebars')
 var router = require('./router.js')
+var nteractions_router = require('./nteractions_router.js')
 
 var port = 8080
+var nteractions_port = 8100
 var partners_network_port = 8090
 
 // connect to database
@@ -25,10 +28,16 @@ app.use("/public", express.static(__dirname + "/public"))
 
 partners_network.engine('.hbs', handlebars({defaultLayout: 'single', extname: '.hbs'}))
 partners_network.set('view engine', '.hbs');
-partners_network.set('port', process.env.PORT || port)
+partners_network.set('port', process.env.PORT || partners_network_port)
 partners_network.use("/public", express.static(__dirname + "/public"))
 
+nteractions.engine('.hbs', handlebars({defaultLayout: 'single', extname: '.hbs'}))
+nteractions.set('view engine', '.hbs');
+nteractions.set('port', process.env.PORT || nteractions_port)
+nteractions.use("/public", express.static(__dirname + "/public"))
+
 app.use(router)
+nteractions.use(nteractions_router)
 partners_network.use(express.Router().get('*', function(req, res, next) {
   res.render('partners-network')
 }))
@@ -39,4 +48,8 @@ app.listen(port, function() {
 
 partners_network.listen(partners_network_port, function() {
 	console.log('partners network on port ' + partners_network_port)
+})
+
+nteractions.listen(nteractions_port, function() {
+	console.log('nteractions on port ' + nteractions_port)
 })
