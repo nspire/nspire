5ba596341ad6e8e6a74cc82e68a18be97a086c07
Dennis Pacewicz <dennis.pacewicz@shopify.com>
2017-01-25 01:25:38 -0500

    glue recent project changes together

--- routes/nteractions_router.js
+++ routes/nteractions_router.js
@@ -16,39 +16,47 @@ router.get('/articles', function(req,res,next) {
 
 router.get('/', function(req,res,next) {
 	article.find({}, (err, articles) => {
-		if(err) res.render('nteractions/home', { layout: 'nteractions'})
+		if(err) return res.render('nteractions/home', { layout: 'nteractions'})
 		res.render('nteractions/home', { layout: 'nteractions', articles: articles})
 	})
 })
 
 router.get('/category/:category', function(req,res,next) {
 	article.find({ "category": req.params.category }, (err, articles) => {
-		if(err) res.render('nteractions/categories', { layout: 'nteractions'})
+		if(err) return res.render('nteractions/categories', { layout: 'nteractions'})
 		res.render('nteractions/categories', { layout: 'nteractions', category: req.params.category, articles: articles})
 	})
 })
 
 router.get('/article/:id', function(req, res, next) {
 	article.find({ "_id": req.params.id }, (err, article) => {
-		if(err) res.render('nteractions/article', { layout: 'nteractions'})
-			console.log(article)
+		if(err) return res.render('nteractions/article', { layout: 'nteractions'})
 		res.render('nteractions/article', { layout: 'nteractions', article: article})
 	})
 })
 
-router.get('/clear', function(req,res,next) {
-	article.remove({}, function (err, result) {
-		if(err) res.json(respond(false, err))
-		res.json(result)
-	})
+router.post('/clear', function(req,res,next) {
+	if(req.body.secret == "nspire2017n") {
+		article.remove({}, function (err, result) {
+			if(err) res.json(respond(false, err))
+			res.json(result)
+		})
+	} else {
+		res.json("permission denied")
+	}
 })
 
 router.post('/article', function(req, res, next) {
 	console.log(req.body)
-	article.create(req.body, function (err, article) {
-		if(err) res.json(respond(false, err))
-		res.json(respond(true, article))
-	})
+	if(req.body.secret == "nspire2017n") {
+		console.log("article created!")
+		article.create(req.body, function (err, article) {
+			if(err) res.json(respond(false, err))
+			res.json(respond(true, article))
+		})
+	} else {
+		res.json("permission denied")
+	}
 })
 
 // 404 handling
